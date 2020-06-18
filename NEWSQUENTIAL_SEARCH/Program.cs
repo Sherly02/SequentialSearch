b04b2f508bc6b4907090688384db7e64ff8e76c0
Sherly <sherly_mauren_25rpl@student.smktelkom-mlg.sch.id>
2020-06-18 08:24:14 +0700

    first commit

---
+++ NEWSQUENTIAL_SEARCH/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace NEWSQUENTIAL_SEARCH
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] Nm = { "DANI", "DINA", "NADI", "DIAN", "ANDI",
+                            "RONI", "ORIN", "NIRO", "IRNO", "NORI",
+                            "MIRA", "ARMI", "IRMA", "IMAR", "RIMA",
+                            "RISA", "SARI", "IRSA", "ARIS", "ASRI",
+                            "ARDI", "DIRA", "RADI", "IRDA", "DIAR",};
+            int Z, N = 30, P = 0;
+
+            for (int I = 0; I < N; I++)
+            { Console.Write(" {0}  ", Nm[I]); }
+
+            Console.Write("\n\n Cari = "); string Cari = Console.ReadLine();
+
+            for (Z = 0; Z < N - 1; Z++)
+                if (Cari == Nm[Z]) { P = Z + 1; break; }
+
+            if (P > 0) Console.Write("\n {0} di posisi ke : {1}", Cari, P);
+            else Console.Write("\n {0} tidak ada...", Cari);
+            Console.ReadKey();
+        }
+    }
+}
